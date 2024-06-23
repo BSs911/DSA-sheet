9ac24adc439734c6d52a156a5acd103c84c5c229
Borse-Shubham <shubhamborse0911@gmail.com>
2024-06-23 20:09:56 +0530

    Largest of Array

---
+++ Array/Easy/LargestElementinanArray.c
@@ -0,0 +1,14 @@
+// C function to find maximum in arr[] of size n
+int largest(int arr[], int n) {
+    
+    int Big = arr[0];
+    
+    for(int i = 0; i < n; i++)
+    {
+        if(Big < arr[i])
+        {
+            Big = arr[i];
+        }
+    }
+    return Big;
+}
