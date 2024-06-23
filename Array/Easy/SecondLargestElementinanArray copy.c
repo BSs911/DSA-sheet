bf7997ee379a75623dd5d52c7681d833daa29cf5
Borse-Shubham <shubhamborse0911@gmail.com>
2024-06-23 21:18:36 +0530

    Second Largest of Array

---
+++ Array/Easy/SecondLargestElementinanArray copy.c
@@ -0,0 +1,23 @@
+class Solution{
+public:	
+	// Function returns the second
+	// largest elements
+	int print2largest(int arr[], int n) {
+	    int Large = arr[0];
+	    int TwoLarge = -1;
+	    
+	    for(int i = 0; i < n; i++)
+	    {
+	        if(arr[i] > Large)
+	        {
+	            TwoLarge = Large;
+	            Large = arr[i];
+	        }
+	        else if(arr[i] < Large && arr[i] > TwoLarge)
+	        {
+	            TwoLarge = arr[i];
+	        }
+	    }
+	    return TwoLarge;
+	}
+};
